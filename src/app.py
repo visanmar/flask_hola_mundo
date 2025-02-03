88fc2bd3573e0c59a06784dc7c0681c6b83feb7b
Vicente L. Sanchis <visanmar@gmail.com>
2025-02-03 13:14:15 +0100

    init

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    username = os.getenv('USER')
+    email = os.getenv('EMAIL')
+    password = os.getenv('PASSWORD')
+
+    print(username, email, password)
+
+    return f'<h1>Mi primera APP Flask deployeada en render.<br>{username}<br>{email}<br>{password}</h1>'
+
+
+def status_404(error):
+    return '<h1>PÁGINA NO ENCONTRADA.</h1>'
+
+
+if __name__ == '__main__':
+    app.register_error_handler(404, status_404)
+    app.run(debug=True)
